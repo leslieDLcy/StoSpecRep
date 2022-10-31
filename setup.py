51fbad46099706643cc95030f2134147d65997bc
leslieDLcy <lesliechen666@outlook.com>
2022-10-31 13:02:06 +0000

    update setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='StoSpecRep',
       version='0.1',
       description='Stochastic spectral representation',
-      url='xx',
+      url='https://github.com/leslieDLcy/StoSpecRep.git',
       author='Leslie',
       author_email='lesliechen@outlook.com',
       license='MIT',
